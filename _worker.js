a17ba43c32452b0fb184d9e5753b47cd38fe2ac6
usegits <zhifu985@gmail.com>
2024-09-25 15:09:15 +0800

    Update _worker.js
    修改uuid

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '502fc5e8-68b5-403c-aa64-b33820b5b11f';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
